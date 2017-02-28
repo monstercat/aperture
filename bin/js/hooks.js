715cd89689c2a3e777ed50295b4fc5c3be7faa25
Colin Kierans <kierans@monstercat.com>
2017-02-27 17:07:50 -0800

    website details dropdown hook

--- bin/js/hooks.js
+++ bin/js/hooks.js
@@ -37,3 +37,44 @@ function hookArtistDropdowns () {
     })
   })
 }
+
+function hookWebsiteDetailsDropdowns () {
+  var selects = findNodes('select[role=website-details-dropdown]')
+
+  if(selects.length == 0) {
+    return
+  }
+
+  selects.forEach(function (el, index) {
+    el.disabled = true
+    el.innerHTML='<option>loading...</option>'
+  })
+
+  requestCached({
+    url: endpoint + '/website/?filters=public,1&fields=name,vanityUri',
+    withCredentials: true
+  }, function (err, data) {
+    if(err) {
+      console.error(err)
+      selects.forEach(function (el, index) {
+        el.disabled = true
+        el.innerHTML='<option>error loading website details</option>'
+      })
+      return
+    }
+
+    var details = data.results.sort(function (a, b) {
+      if(a.name == b.name) return 0
+      return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+    })
+
+    var optionsHTML = details.map(function (detail) {
+      return '<option value="' + detail._id + '">' + detail.name + ' / ' + detail.vanityUri  + '</option>'
+    }).join("\n")
+
+    selects.forEach(function (el, index) {
+      el.disabled = false
+      el.innerHTML = "<option>-select website details-</option>\n" + optionsHTML
+    })
+  })
+}
