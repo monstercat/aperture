94d0cab6e12c826b720aabe7c09d559d5eb421d3
Colin Kierans <kierans@monstercat.com>
2017-03-24 11:42:29 -0700

    datetime helper

--- bin/js/handlebars-helpers.js
+++ bin/js/handlebars-helpers.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   if(window.Handlebars) {
-    console.log('register hler')
     Handlebars.registerHelper('date', function (val) {
       return new Handlebars.SafeString('<abbr class="date" title="' + new Date(val).toISOString() + '">' + formatDate(val) + '</abbr>')
+    })
+
+    Handlebars.registerHelper('datetime', function (val) {
+      return new Handlebars.SafeString('<abbr class="date" title="' + new Date(val).toISOString() + '">' + formatDateTime(val) + '</abbr>')
     })
   }
 })
