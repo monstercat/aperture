25ff1d43c0ea428136e3c07318c08f772e391151
Colin Kierans <kierans@monstercat.com>
2017-03-27 14:34:41 -0700

    Error checking in date helpers

--- bin/js/handlebars-helpers.js
+++ bin/js/handlebars-helpers.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   if(window.Handlebars) {
     Handlebars.registerHelper('date', function (val) {
-      return new Handlebars.SafeString('<abbr class="date" title="' + new Date(val).toISOString() + '">' + formatDate(val) + '</abbr>')
+      if(!val) {
+        return new Handlebars.SafeString('<abbr class="invalid-date">no date</abbr>')
+      }
+      var dateStr = new Date(val).toISOString() 
+      return new Handlebars.SafeString('<abbr class="date" title="' + dateStr + '">' + formatDate(val) + '</abbr>')
     })
 
     Handlebars.registerHelper('datetime', function (val) {
+      if(!val) {
+        return new Handlebars.SafeString('<abbr class="invalid-date">no datetime</abbr>')
+      }
       return new Handlebars.SafeString('<abbr class="date" title="' + new Date(val).toISOString() + '">' + formatDateTime(val) + '</abbr>')
     })
   }
