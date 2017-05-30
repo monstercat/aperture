92e26f9d365a5050ed33b32d4da7895125c2b193
Colin Kierans <kierans@monstercat.com>
2017-05-29 17:00:12 -0700

    Added country dropdown hook.

--- bin/js/hooks.js
+++ bin/js/hooks.js
@@ -95,13 +95,26 @@ function hookWebsiteDetailsDropdowns (node) {
 function hookTrackDropdowns (node) {
   return hookRemoteDropdowns(node, {
     selectRole: 'track-dropdown',
-    errMsg: 'error website details',
+    errMsg: 'error loading tracks',
     url: endpoint + '/track/?fields=title,artistsTitle',
     sortField: 'title',
     getLabel: function (obj) {
       return obj.title + ' by ' + obj.artistsTitle
     },
     promptMsg: '-select track-'
+  })
+}
+
+function hookReleaseDropdowns (node) {
+  return hookRemoteDropdowns(node, {
+    selectRole: 'release-dropdown',
+    errMsg: 'error loading releases',
+    url: endpoint + '/release/?fields=title,renderedArtists',
+    sortField: 'title',
+    getLabel: function (obj) {
+      return obj.title + ' by ' + obj.renderedArtists
+    },
+    promptMsg: '-select release-'
   })
 }
 
@@ -178,4 +191,14 @@ function hookDateFields (node) {
     var e = new Event('keyup')
     el.dispatchEvent(e)
   })
+}
+
+var countries = ["Afghanistan","Åland","Albania","Algeria","American Samoa","Andorra","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Christmas Island","Cocos (Keeling) Islands","Colombia","Comoros","Congo (Brazzaville)","Congo (Kinshasa)","Cook Islands","Costa Rica","Côte d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Guiana","French Polynesia","French Southern Lands","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea-Bissau","Guyana","Haiti","Heard and McDonald Islands","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Korea North","Korea South","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","Northern Mariana Islands","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Pitcairn","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russian Federation","Rwanda","Saint Barthélemy","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Martin (French part)","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and South Sandwich Islands","Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen Islands","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor-Leste","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States Minor Outlying Islands","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands British","Virgin Islands U.S.","Wallis and Futuna Islands","Western Sahara","Yemen","Zambia","Zimbabwe"];
+function hookCountrySelects (selects) {
+  selects = selects || findNodes('select[role=country]')
+  selects.forEach(function (sel) {
+    sel.innerHTML = '<option></option>' + countries.map(function (c) {
+      return '<option value="' + c + '">' + c + '</option>';
+    });
+  });
 }
